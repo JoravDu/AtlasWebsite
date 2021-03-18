eb9063d9c3a66ea30dce1d6d824615634ca70446
JoravDu <53528905+JoravDu@users.noreply.github.com>
2021-03-18 19:11:32 +0100

    1.2

--- js/index.js
+++ js/index.js
@@ -1,17 +1,57 @@
 import 'ol/ol.css';
-import {Map, View} from 'ol';
-import TileLayer from 'ol/layer/Tile';
-import OSM from 'ol/source/OSM';
+import Circle from 'ol/geom/Circle';
+import Feature from 'ol/Feature';
+import GeoJSON from 'ol/format/GeoJSON';
+import Map from 'ol/Map';
+import View from 'ol/View';
+import {Circle as CircleStyle, Fill, Stroke, Style} from 'ol/style';
+import {OSM, Vector as VectorSource} from 'ol/source';
+import {Tile as TileLayer, Vector as VectorLayer} from 'ol/layer';
+import Projection from 'ol/proj/Projection';
 
-const map = new Map({
-  target: 'map',
+var styles = {
+  'Polygon': new Style({
+    stroke: new Stroke({
+      color: 'blue',
+      lineDash: [5],
+      width: 3,
+    }),
+    fill: new Fill({
+      color: 'rgba(0, 0, 255, 0.1)',
+    }),
+  }),
+};
+
+var styleFunction = function (feature) {
+  return styles[feature.getGeometry().getType()];
+};
+
+const data = require('/data/geojson.json')
+var geojsonObject = data;
+
+var vectorSource = new VectorSource({
+    features: (new GeoJSON()).readFeatures(geojsonObject,
+            {
+            dataProjection: 'EPSG:4326',
+            featureProjection: 'EPSG:3857'
+            })
+    });
+
+
+var vectorLayer = new VectorLayer({
+  source: vectorSource,
+  style: styleFunction,
+});
+
+var map = new Map({
   layers: [
     new TileLayer({
-      source: new OSM()
-    })
-  ],
+      source: new OSM(),
+    }),
+    vectorLayer ],
+  target: 'map',
   view: new View({
     center: [0, 0],
-    zoom: 0
-  })
+    zoom: 2,
+  }),
 });
