2c19c0da9f130f2ed1185d0bede42eb1ec4eed4f
JoravDu <53528905+JoravDu@users.noreply.github.com>
2021-03-21 18:24:10 +0100

    new update

--- dist/chartjs.938960d4.js
+++ dist/chartjs.938960d4.js
@@ -119,24 +119,147 @@ parcelRequire = (function (modules, cache, entry, globalName) {
   return newRequire;
 })({"js/chartjs/chartjs.js":[function(require,module,exports) {
 window.addEventListener('load', function (event) {
-  var ctx = document.getElementById('myChart').getContext('2d');
-  var myChart = new Chart(ctx, {
-    type: 'bar',
+  var ctx = document.getElementById('myChart');
+  var chart = new Chart(ctx, {
+    type: 'bubble',
     data: {
-      labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+      labels: "Zon",
       datasets: [{
-        label: '# of Votes',
-        data: [12, 19, 3, 5, 2, 3],
-        backgroundColor: ['rgba(255, 99, 132, 0.2)', 'rgba(54, 162, 235, 0.2)', 'rgba(255, 206, 86, 0.2)', 'rgba(75, 192, 192, 0.2)', 'rgba(153, 102, 255, 0.2)', 'rgba(255, 159, 64, 0.2)'],
-        borderColor: ['rgba(255, 99, 132, 1)', 'rgba(54, 162, 235, 1)', 'rgba(255, 206, 86, 1)', 'rgba(75, 192, 192, 1)', 'rgba(153, 102, 255, 1)', 'rgba(255, 159, 64, 1)'],
-        borderWidth: 1
+        label: ["Lissabon"],
+        backgroundColor: "rgba(166,206,227,1)",
+        borderColor: "rgba(166,206,227,1)",
+        data: [{
+          x: 38.68,
+          y: 2799,
+          r: 30
+        }]
+      }, {
+        label: ["Londen"],
+        backgroundColor: "rgba(31,120,180,1)",
+        borderColor: "rgba(31,120,180,1)",
+        data: [{
+          x: 51.52,
+          y: 1410,
+          r: 30
+        }]
+      }, {
+        label: ["Amsterdam"],
+        backgroundColor: "rgba(178,223,138,0.2)",
+        borderColor: "rgba(178,223,138,1)",
+        data: [{
+          x: 52.36,
+          y: 1524,
+          r: 15
+        }]
+      }, {
+        label: ["Maastricht"],
+        backgroundColor: "rgba(51,160,44,0.2)",
+        borderColor: "rgba(51,160,44,1)",
+        data: [{
+          x: 50.84,
+          y: 1703,
+          r: 15
+        }]
+      }, {
+        label: ["Marseille"],
+        backgroundColor: "rgba(251,154,153,0.2)",
+        borderColor: "rgba(251,154,153,1)",
+        data: [{
+          x: 43.28,
+          y: 2858,
+          r: 15
+        }]
+      }, {
+        label: ["Madrid"],
+        backgroundColor: "rgba(227,26,28,0.2)",
+        borderColor: "rgba(227,26,28,1)",
+        data: [{
+          x: 40.43,
+          y: 2769,
+          r: 15
+        }]
+      }, {
+        label: ["Barcelona"],
+        backgroundColor: "rgba(253,191,111,0.2)",
+        borderColor: "rgba(253,191,111,1)",
+        data: [{
+          x: 41.39,
+          y: 2524,
+          r: 15
+        }]
+      }, {
+        label: ["Brussel"],
+        backgroundColor: "rgba(255,127,0,0.2)",
+        borderColor: "rgba(255,127,0,1)",
+        data: [{
+          x: 50.85,
+          y: 1546,
+          r: 15
+        }]
+      }, {
+        label: ["Athene"],
+        backgroundColor: "rgba(202,178,214,0.2)",
+        borderColor: "rgba(202,178,214,1)",
+        data: [{
+          x: 37.99,
+          y: 2771,
+          r: 15
+        }]
+      }, {
+        label: ["Dublin"],
+        backgroundColor: "rgba(106,61,154,0.2)",
+        borderColor: "rgba(106,61,154,1)",
+        data: [{
+          x: 53.32,
+          y: 1424,
+          r: 15
+        }]
+      }, {
+        label: ["Hamburg"],
+        backgroundColor: "rgba(255,255,153,0.5)",
+        borderColor: "rgba(255,255,153,1)",
+        data: [{
+          x: 53.55,
+          y: 1557,
+          r: 15
+        }]
+      }, {
+        label: ["Birmingham"],
+        backgroundColor: "rgba(177,89,40,0.2)",
+        borderColor: "rgba(177,89,40,1)",
+        data: [{
+          x: 52.47,
+          y: 1364,
+          r: 15
+        }]
       }]
     },
     options: {
+      tooltips: {
+        callbacks: {
+          label: function label(tooltipItem, data) {
+            city = data.datasets[tooltipItem.datasetIndex].label[0];
+            return city + ': noorderbreedte van deze plaats is ' + tooltipItem.xLabel + ' met het aantal zonuren: ' + tooltipItem.yLabel;
+          }
+        }
+      },
+      title: {
+        display: true,
+        text: 'Zonuren en ligging op de aarde'
+      },
       scales: {
         yAxes: [{
-          ticks: {
-            beginAtZero: true
+          scaleLabel: {
+            display: true,
+            labelString: "Zonuren",
+            position: 'left'
+          }
+        }],
+        xAxes: [{
+          scaleLabel: {
+            display: true,
+            labelString: "Noorderbreedte",
+            position: 'right'
           }
         }]
       }
@@ -171,7 +294,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "51844" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "56056" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
